d51af9ffecca2e1c6400125b5177c6b3fefdf1e5
bintangx1902 <60544749+bintangx1902@users.noreply.github.com>
2020-04-25 16:41:12 +0700

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -5,8 +5,8 @@ app_name = 'core'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('profile', profile_view, name='profile'),
-    path('register', register_view, name='register'),
+    path('profile/', profile_view, name='profile'),
+    path('register/', register_view, name='register'),
     path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
